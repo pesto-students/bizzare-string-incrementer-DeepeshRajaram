eaad4ca2dd853265af0d240f5eb1f2f5e5a44bb3
Deepesh Rajaram <deepeshkumar1988@live.com>
2020-12-14 17:07:52 +0530

    Update bizarreStringIncrementer.js
    function

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,33 @@
 // Start your implementation here
+bizarreStringIncrementer = (arg) =>{
+    let arrString = arg.split('');
+    let lastNumber= new Array();
+    let numData = "";
+    let numINT;
+    let strRemaining = "";
+    for(let a in arrString)
+    {
+        if(!isNaN(arrString[a]))
+        {
+            lastNumber.push(arrString[a]);
+            numData = numData + arrString[a];
+        }
+        else
+        {
+            strRemaining = strRemaining + arrString[a];
+        }
+    }
+    if(!isNaN(numData))
+    {
+        numINT = parseInt(numData);
+        numINT= ++numINT;
+    }
+    strRemaining = strRemaining + numINT;
+
+    console.log(lastNumber);
+    console.log(numData);
+    console.log(numINT);
+    console.log(strRemaining);
+}
+
+bizarreStringIncrementer('abc58');
