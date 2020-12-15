04935aa575e2096a050c43d56217c705e13f72e2
Deepesh Rajaram <deepeshkumar1988@live.com>
2020-12-15 21:13:33 +0530

    Update bizarreStringIncrementer.js
    More scenarions handled

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -5,6 +5,8 @@ bizarreStringIncrementer = (arg) =>{
     let numData = "";
     let numINT;
     let strRemaining = "";
+    let continueousNum = false;
+
     for(let a in arrString)
     {
         if(!isNaN(arrString[a]))
@@ -12,22 +14,36 @@ bizarreStringIncrementer = (arg) =>{
             lastNumber.push(arrString[a]);
             numData = numData + arrString[a];
         }
-        else
-        {
-            strRemaining = strRemaining + arrString[a];
+        else{
+            numData = new Array();
         }
     }
     if(!isNaN(numData))
     {
         numINT = parseInt(numData);
-        numINT= ++numINT;
+        numINT = ++numINT;
     }
-    strRemaining = strRemaining + numINT;
+    for(let i=0;i<numData.length;i++){
+        arrString.pop();
+    }
 
-    console.log(lastNumber);
-    console.log(numData);
-    console.log(numINT);
+    for(let a in arrString)
+    {
+        strRemaining = strRemaining.concat(arrString[a]);
+    }
+    if(numData.length>0){
+        strRemaining  = strRemaining.concat(numINT);
+    }
+    else{
+        strRemaining = strRemaining.concat("1");
+    }
+
+
+    // console.log(lastNumber);
+    // console.log(numData);
+    // console.log(numINT);
     console.log(strRemaining);
 }
 
-bizarreStringIncrementer('abc58');
+bizarreStringIncrementer('a12an');
+bizarreStringIncrementer('a12an21');
